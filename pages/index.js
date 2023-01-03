5bbca431ee0c1804ec1e3c3595719ea0d9e39128
Renzo Samuel García Cervera <renzogc745@gmail.com>
2023-01-02 20:30:19 -0500

    feat: actualizar información del head de la web

--- pages/index.js
+++ pages/index.js
@@ -4,8 +4,8 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Create Next App</title>
-        <meta name="description" content="Dashboard" />
+        <title>RG Dashboard</title>
+        <meta name="description" content="Dashboard para uso personal" />
         <meta name="author" content="Renzo García" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
