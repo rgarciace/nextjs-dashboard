22f70b9fea96b913367c8a90c3371532b57cb3a5
Renzo Samuel García Cervera <renzogc745@gmail.com>
2023-01-03 09:43:55 -0500

    feat: crear componente Header

---
+++ components/Header.js
@@ -0,0 +1,16 @@
+const Header = ({ title }) => {
+  return (
+    <div>
+      <div>
+        <h2>{title}</h2>
+        <div>Opciones</div>
+      </div>
+      <div>
+        <div>Barra de búsqueda</div>
+        <div>Usuario</div>
+      </div>
+    </div>
+  )
+}
+
+export default Header
