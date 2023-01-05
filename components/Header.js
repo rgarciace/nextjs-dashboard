f5b99031d0d4bc07fa605b8ba2c684f3c9be3d24
Renzo Samuel García Cervera <renzogc745@gmail.com>
2023-01-04 22:58:36 -0500

    feat: añadir responsive al header

--- components/Header.js
+++ components/Header.js
@@ -1,16 +1,59 @@
-import { Search } from 'iconoir-react';
+import { Menu, Search } from 'iconoir-react';
 import Link from 'next/link'
 import { useRouter } from "next/router";
+import { useEffect, useState } from 'react';
 import styles from '/styles/components/Header.module.css'
 const Header = ({ title = "Inicio", options = [] }) => {
   const router = useRouter();
+
+  const [openMenu, setOpenMenu] = useState(true);
+  const [openSearchBar, setOpenSearchBar] = useState(false);
+
+  const handlerMobileMenu = () => {
+    setOpenSearchBar(false)
+    setOpenMenu(!openMenu)
+  }
+  const handlerMobileSearchBar = () => {
+    setOpenMenu(false)
+    setOpenSearchBar(!openSearchBar)
+  }
+
+  useEffect(() => {
+    setOpenMenu(false)
+    setOpenSearchBar(false)
+  }, [router])
+
+
   return (
     <div className={styles.centered}>
       <div className={styles.container}>
         <section className={styles.information}>
           <h2 className={styles.title}>{title}</h2>
           <div className={styles.others}>
-            <button className={styles.buttonSearch}><Search /></button>
+            {
+              options.length > 0 && (
+                <>
+                  <button onClick={handlerMobileMenu} className={styles.buttonMobile}><Menu /></button>
+                  <div className={`${styles.mobileMenu} ${openMenu && styles.mobileMenuActive}`}>
+                    <nav className={styles.mobileNav}>
+                      {options.map(({ text, path }) => (
+                        <Link
+                          className={`${styles.option} ${(router.pathname == path || router.pathname.includes(path + "/")) && styles.optionActive}`}
+                          key={text}
+                          href={path}>
+                          {text}
+                        </Link>
+                      ))}
+                    </nav>
+                  </div>
+                </>
+              )
+            }
+            <button onClick={handlerMobileSearchBar} className={styles.buttonMobile}><Search /></button>
+
+            <div className={`${styles.mobileSearchBar} ${openSearchBar && styles.mobileSearchBarActive}`}>
+              <input type="search" className={styles.mobileInputSearch} placeholder="Buscara aquí..." autoFocus />
+            </div>
             <div className={styles.inputSearch}>
               <Search />
               <input className={styles.searchBar} type="search" placeholder='Buscar aquí...' />
@@ -19,16 +62,20 @@ const Header = ({ title = "Inicio", options = [] }) => {
             </div>
           </div>
         </section>
-        <nav className={styles.optionsGroup}>
-          {options.map(({ text, path }) => (
-            <Link
-              className={`${styles.option} ${(router.pathname == path || router.pathname.includes(path + "/")) && styles.optionActive}`}
-              key={text}
-              href={path}>
-              {text}
-            </Link>
-          ))}
-        </nav>
+        {
+          options.length > 0 && (
+            <nav className={styles.optionsGroup}>
+              {options.map(({ text, path }) => (
+                <Link
+                  className={`${styles.option} ${(router.pathname == path || router.pathname.includes(path + "/")) && styles.optionActive}`}
+                  key={text}
+                  href={path}>
+                  {text}
+                </Link>
+              ))}
+            </nav>
+          )
+        }
       </div>
     </div>
   )
