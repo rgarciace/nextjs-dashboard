5873ef97a180d85c853daae9fdbafe0ea53d2e1d
Renzo Samuel García Cervera <renzogc745@gmail.com>
2023-01-04 20:06:57 -0500

    feat agregar estilos al header

--- components/Header.js
+++ components/Header.js
@@ -1,13 +1,34 @@
-const Header = ({ title }) => {
+import { Search } from 'iconoir-react';
+import Link from 'next/link'
+import { useRouter } from "next/router";
+import styles from '/styles/components/Header.module.css'
+const Header = ({ title = "Inicio", options = [] }) => {
+  const router = useRouter();
   return (
-    <div>
-      <div>
-        <h2>{title}</h2>
-        <div>Opciones</div>
-      </div>
-      <div>
-        <div>Barra de búsqueda</div>
-        <div>Usuario</div>
+    <div className={styles.centered}>
+      <div className={styles.container}>
+        <section className={styles.information}>
+          <h2 className={styles.title}>{title}</h2>
+          <div className={styles.others}>
+            <button className={styles.buttonSearch}><Search /></button>
+            <div className={styles.inputSearch}>
+              <Search />
+              <input className={styles.searchBar} type="search" placeholder='Buscar aquí...' />
+            </div>
+            <div className={styles.user}>
+            </div>
+          </div>
+        </section>
+        <nav className={styles.optionsGroup}>
+          {options.map(({ text, path }) => (
+            <Link
+              className={`${styles.option} ${(router.pathname == path || router.pathname.includes(path + "/")) && styles.optionActive}`}
+              key={text}
+              href={path}>
+              {text}
+            </Link>
+          ))}
+        </nav>
       </div>
     </div>
   )
