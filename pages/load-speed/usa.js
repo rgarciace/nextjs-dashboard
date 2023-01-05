16f5958af4f668846a2241112a908b660367de8c
Renzo Samuel García Cervera <renzogc745@gmail.com>
2023-01-05 00:00:04 -0500

    feat: crear páginas Velocidad de Carga USA

---
+++ pages/load-speed/usa.js
@@ -0,0 +1,32 @@
+import Head from 'next/head'
+import Footer from '/components/Footer'
+import styles from '/styles/Home.module.css'
+import Header from '../../components/Header'
+
+const options = [
+  { text: "Velocidad CAN", path: "/load-speed/can" },
+  { text: "Velocidad USA", path: "/load-speed/usa" },
+  { text: "Reportes", path: "/load-speed/reportes" },
+]
+
+const LoadSpeedUsa = () => {
+  return (
+    <>
+      <Head>
+        <title>RG Velocidad USA</title>
+        <meta name="description" content="Dashboard para uso personal" />
+        <meta name="author" content="Renzo García" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div>
+        <main className="main">
+          <Header title="Velocidad de Carga" options={options} />
+        </main>
+        <Footer />
+      </div>
+    </>
+  )
+}
+
+export default LoadSpeedUsa
