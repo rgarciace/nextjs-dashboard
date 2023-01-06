3294bc5257f9605c4d71146adcb3e319fc0c2f1d
Renzo Samuel García Cervera <renzogc745@gmail.com>
2023-01-05 23:42:44 -0500

    feat: simular base de datos en formato JS

---
+++ utils/bbdd.js
@@ -0,0 +1,11 @@
+export const websTrackingDB = [
+  { name: "Coolbox", url: "coolbox.pe" },
+  { name: "B&H", url: "bhphotovideo.com" },
+  { name: "Cellcomputers", url: "cellcomputersbd.com" },
+  { name: "Diners", url: "dinersclubmall.pe" },
+  { name: "Falabella", url: "falabella.com.pe" },
+  { name: "Memory Kings", url: "memorykings.pe" },
+  { name: "Mercado Libre", url: "mercadolibre.com.pe" },
+  { name: "Mesajil", url: "mesajil.com" },
+  { name: "Oechsle", url: "oechsle.pe" },
+]
