94fce7207c8faccc64cbf7e71f51dc6ae04aacc9
Renzo Samuel García Cervera <renzogc745@gmail.com>
2023-01-10 22:47:56 -0500

    feat: completar sección Web en Seguimiento para Velocidad CAN

--- pages/load-speed/can.js
+++ pages/load-speed/can.js
@@ -6,37 +6,37 @@ import styles from '/styles/load-speed/Can.module.css'
 import SectionInformation from '../../components/SectionInformation'
 
 import { websTrackingDB } from '/utils/bbdd'
-import { Trash } from 'iconoir-react'
+import ListWebs from '../../components/ListWebs'
+import HistoryColors from '../../components/HistoryColors'
 
 export default function LoadSpeedCan() {
   const [openModalAdd, setOpenModalAdd] = useState(false)
+  const [webFocus, setWebFocus] = useState("Coolbox");
   const addWebForTracking = () => {
     setOpenModalAdd(true)
   }
   return (
     <>
       <MainLayout title="Velocidad de CAN" titleHeader="Velocidad de Carga" options={pathLoadSpeed}>
-        <section className={styles.section}>
-          <header className={styles.headerSection}>
-            <SectionInformation title="Webs en seguimiento" description="Ecommerce en seguimiento para realizar comparaciones" />
-            <ButtonAdd add={addWebForTracking} />
-          </header>
-          <div className={styles.bodySection}>
-            <table>
-              <tbody>
-                {
-                  websTrackingDB.map(({ name, url }) => (
-                    <tr className={styles.webTracking}>
-                      <td className={styles.nameWeb}>{name}</td>
-                      <td className={styles.urlWeb}>{url}</td>
-                      {name == "Coolbox" || <Trash />}
-                    </tr>
-                  ))
-                }
-              </tbody>
-            </table>
+        <div className={styles.main}>
+          <div className={styles.containerHalf}>
+            <header className={styles.headerSection}>
+              <SectionInformation title="Última actividad" description={`Últimas 5 mediciones a la web de ${webFocus}`} />
+              <ButtonAdd add={addWebForTracking} />
+            </header>
+            <HistoryColors first="Carga completa" second='Primera interacción' />
+            
           </div>
-        </section>
+          <section className={styles.containerHalf}>
+            <header className={styles.headerSection}>
+              <SectionInformation title="Webs en seguimiento" description="Ecommerce en seguimiento para realizar comparaciones" />
+              <ButtonAdd add={addWebForTracking} />
+            </header>
+            <div className={styles.bodySection}>
+              <ListWebs websTrackingDB={websTrackingDB} />
+            </div>
+          </section>
+        </div>
       </MainLayout>
     </>
   )
