7b9141358118cddf7b9d3450b4ecfd44ed9df40d
Renzo Samuel García Cervera <renzogc745@gmail.com>
2023-03-11 19:12:44 -0500

    feat: agregar estilos de Velocidad CAN

--- pages/carriers/update-data.js
+++ pages/carriers/update-data.js
@@ -1,10 +1,11 @@
 import MainLayout from '/components/MainLayout'
+import styles from '/styles/carriers/UpdateData.module.css'
 
 const UpdateData = () => {
   return (
-    <MainLayout title="Dashboard Cool" titleHeader="Actualizar Data">
-      <input type="file" />
-      <select>
+    <MainLayout title="Dashboard Cool" titleHeader="Transportadoras">
+      <input className={styles.inputFile} type="file" accept='.xls'/>
+      <select className={styles.selectType}>
         <option value="sameday" key="">Entrega hoy</option>
         <option value="nextday" key="">24 horas</option>
         <option value="lima" key="">Lima</option>
